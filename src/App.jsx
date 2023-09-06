cd030ab6a24a8520a2c053bb600e87b0478fb0b7
Anthony DelVecchio <amdelvecchio1225@gmail.com>
2023-09-06 14:14:56 -0400

    complete

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Home from './components/Home'
+import Red from './components/Red'
+import Blue from './components/Blue'
+import { Routes, Route, Link } from "react-router-dom"
+
+function App() {
+  return (
+  <div id="container">
+      <div id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+        </Routes>
+      </div>
+  </div>
+  )
+}
+
+export default App
